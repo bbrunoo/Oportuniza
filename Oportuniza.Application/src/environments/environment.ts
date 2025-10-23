32296a5acb81a6c9151683b7113bbb456eec4d45
bruno-santos <bruno.santos384@fatec.sp.gov.br>
2025-10-22 21:18:42 -0300

    changed config

--- Oportuniza.Application/src/environments/environment.ts
+++ Oportuniza.Application/src/environments/environment.ts
@@ -4,10 +4,10 @@ export const environment = {
     uri: 'http://localhost:5000/',
   },
   keycloak: {
-    url: 'https://keycloak.oportuniza.site',
+    url: 'https://auth.oportuniza.site',
     realm: 'oportuniza',
     clientId: 'oportuniza-client',
-    secret:"RMNT9CAC1mHULvD5vhhyg80NlFX1Kftt"
+    secret:"yfq4temRzipAqpkQvobcCY78ES7wH3G4"
   },
   apiUrl: "http://localhost:5000/api",
   apiUrlNormal: "http://localhost:5000"
